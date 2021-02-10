66f0e9455ee8c8cb55379033c334e5f27d479200
Cristian Manzano Rodriguez <cristian@MacBook-Pro-de-Cristian.local>
2021-02-10 03:18:43 +0100

    adding home screen layout

---
+++ App.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import {StyleSheet, View, Image, Text, ScrollView} from 'react-native';
+
+const App = () => {
+  return (
+    <ScrollView>
+      <View style={{flexDirection: 'row'}}>
+        <Image style={styles.banner} source={require('./assets/img/bg.jpg')} />
+      </View>
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Qué hacer en París</Text>
+
+        <ScrollView horizontal>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad1.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad2.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad3.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad4.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad5.jpg')}
+            />
+          </View>
+        </ScrollView>
+
+        <Text style={styles.title}>Los mejores alojamientos</Text>
+        <View>
+          <View>
+            <Image
+              style={styles.mejores}
+              source={require('./assets/img/mejores1.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.mejores}
+              source={require('./assets/img/mejores2.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.mejores}
+              source={require('./assets/img/mejores3.jpg')}
+            />
+          </View>
+        </View>
+        <Text style={styles.title}>Hoteles en LA</Text>
+        <View 
+          style={styles.listado}
+        >
+          <View style={styles.item}>
+            <Image
+              style={styles.mejores}
+              source={require('./assets/img/hospedaje1.jpg')}
+            />
+          </View>
+          <View style={styles.item}>
+            <Image
+              style={styles.mejores}
+              source={require('./assets/img/hospedaje2.jpg')}
+            />
+          </View>
+          <View style={styles.item}>
+            <Image
+              style={styles.mejores}
+              source={require('./assets/img/hospedaje3.jpg')}
+            />
+          </View>
+          <View style={styles.item}>
+            <Image
+              style={styles.mejores}
+              source={require('./assets/img/hospedaje4.jpg')}
+            />
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  banner: {
+    height: 250,
+    flex: 1,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20,
+  },
+  container: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
+  },
+  mejores: {
+    width: '100%',
+    height: 200,
+    marginVertical: 5,
+  },
+  listado:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  item: {
+    flexBasis: '49%',
+  },
+});
+
+export default App;
